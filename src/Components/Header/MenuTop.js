6f78d9be31a240d09cbe7643eb651eeb3341eaee
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2022-09-08 12:17:43 +0300

    redesign of battle sites

--- src/Components/Header/MenuTop.js
+++ src/Components/Header/MenuTop.js
@@ -108,9 +108,18 @@ const MenuTop = ({mapRef, setBurgerActive}) => {
                    {item.name}
                 </li>
               ))}
+              <li
+                className={'menu-sub__item'}
+              >
+                 Балаклея
+              </li>
+              <li
+                className={'menu-sub__item'}
+              >
+                 Херсон
+              </li>
            </ul>}
         </li>
-
      </ul>
    );
 };
