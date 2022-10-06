2dd3eff9ec1dcc4249b984808c0ff1b38d66985c
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2022-10-06 12:42:52 +0300

    add legend map

--- src/Components/MapLegend/MapLegend.js
+++ src/Components/MapLegend/MapLegend.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './mapLegend.scss'
+import axios from "axios";
 
 export const MapLegend = () => {
+
+   const [iconsLegend, setIconsLegend] = useState([])
+
+   useEffect(async ()=>{
+      const response = await axios.get(`${process.env.REACT_APP_BASE_URL}event-type/get-event-types`)
+      setIconsLegend(response.data.data)
+   }, [])
+
    return (
      <div className={'legend'}>
-        <div className="legend__color__control">
+        <div className="legend__control">
            <div className="legend__title">Зоны контроля</div>
            <div className="legend__row">
               <div className="legend__color legend__color_red" />
@@ -15,28 +24,17 @@ export const MapLegend = () => {
               <div className="legend__none">Захваченные территории</div>
            </div>
         </div>
-        <div className="legend__color__icons">
-           <div className="legend__title">Иконки</div>
-           <div className="legend__row">
-              <div className="legend__icon">
-                 <img src="https://front.dnr.one//uploads/news-image/icon/d4e267c58b9141a3198357923d8fdca5.png" alt="icon"/>
-              </div>
-              <div className="legend__none">Международное событие</div>
-           </div>
-           <div className="legend__row">
-              <div className="legend__icon">
-                 <img src="https://front.dnr.one//uploads/news-image/icon/59f4e717cced81c196a0c51d94377e7c.png" alt="icon"/>
-              </div>
-              <div className="legend__none">Поставка вооружений</div>
-           </div>
-           <div className="legend__row">
-              <div className="legend__icon">
-                 <img src="https://front.dnr.one//uploads/news-image/icon/07f3df888a067dbb88e214ddcedfe9e5.svg" alt="icon"/>
-              </div>
-              <div className="legend__none">Обстрел</div>
-           </div>
+        <div className="legend__title">Иконки</div>
+        <div className="legend__icons">
+           {iconsLegend?.map(item=>(
+             <div className="legend__row" key={item.id}>
+                <div className="legend__icon">
+                   <img src={`https://front.dnr.one/${item.icon}`} alt={"icon"+item.id}/>
+                </div>
+                <div className="legend__none">{item.title}</div>
+             </div>
+           ))}
         </div>
-
      </div>
    );
 };
